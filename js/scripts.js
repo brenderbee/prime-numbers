d2fb32b1edff4395ae8224e590f470afe8ae813b
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-03-31 22:26:09 -0700

    Correct spelling.

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ function prime(number) {
 }
 
 
-// User-Interface Logicd
+// User-Interface Logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
